dbb7f9c1af93867af1681bc2fca1ca0886ed3f5a
frem01 <116328627+frem01@users.noreply.github.com>
2022-10-21 11:07:39 +0700

    frem
    555

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("Myname is frem");
+        System.out.println("55");
     }
 }
